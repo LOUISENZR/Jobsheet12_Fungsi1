adfc63e631c787ddf8114bbee76f12e1144e130f
LOUISENZR <louisanaza@gmail.com>
2023-11-23 08:35:00 +0700

    First

---
+++ TerimaKasih_16.java
@@ -0,0 +1,19 @@
+/**
+ * TerimaKasih_16
+ */
+public class TerimaKasih_16 {
+        public static void UcapanTerimakasih(){
+            System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like in could ask you anything.");
+        }
+        public static void UcapanTambahan(String ucapan){
+            System.out.println(ucapan);
+        }
+    
+    public static void main(String[] args) {
+        UcapanTerimakasih();
+        String ucapan = "Terima Kasih Pal...Bu...Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
+    }
+    
+
+}
